3c971ffcb8713cf36bea02b1d4d262e1063a0fce
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-09-02 17:13:05 -0600

    Se agrego el composable Onboarding

--- MiHolaMundoo/app/src/main/java/net/ivanvega/miholamundoo/MainActivity.kt
+++ MiHolaMundoo/app/src/main/java/net/ivanvega/miholamundoo/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.ivanvega.miholamundoo.ui.theme.MiHolaMundooTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,14 +30,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            
             MiHolaMundooTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Perro",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp( modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Onboardingscreen( modifier: Modifier = Modifier ){
+    val sholudShowOnBoarding = remember {
+        mutableStateOf(true)
+    }
+    Column {
+        Text(text = "Welcome to the basics compose" )
+        Button(onClick = { sholudShowOnBoarding.value = false }) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewOnboardingscreen(){
+    MiHolaMundooTheme {
+        Onboardingscreen()
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier =  Modifier,
+          names : List<String> = listOf("Mundo","Android", "Compose")
+){
+    Surface (
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+
+        Column (modifier = modifier.padding(vertical = 4.dp)) {
+            for (  name in names  ){
+                Greeting(name = name)
             }
         }
     }
@@ -37,25 +79,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-
     /*Surface( content = {
 
         })*/
-
-    Surface (color = MaterialTheme .colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    //var expanded = false
+    var expanded = remember {
+         mutableStateOf(false)
     }
-
-
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface (color = MaterialTheme .colorScheme.primary
+        , modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row (modifier = Modifier
+            .padding(24.dp)
+            .padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(
+                    text = "Hello ",
+                )
+                Text(
+                    text = "$name!",
+                )
+            }
+            ElevatedButton(onClick =
+                {  expanded.value = !expanded.value   }
+            ) {
+                Text(text =  if (expanded.value) "Show less" else "Show more"   )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MiHolaMundooTheme {
-        Greeting("Gato")
+        MyApp()
     }
 }
