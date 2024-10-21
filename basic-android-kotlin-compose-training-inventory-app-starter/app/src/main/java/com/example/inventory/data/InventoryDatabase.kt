fd8d6239e5e880191b3c8e13c71081e4a6c92642
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-10-21 17:18:04 -0600

    Se genero el dao Se genero la base de datos

---
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,22 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Item::class], version = 1 )
+public abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+        fun getDatabase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
---
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,28 @@
+package com.example.inventory.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+
+    @Insert
+    suspend fun insertPerro(item: Item)
+
+    @Update
+    suspend fun perroActualizar(item: Item)
+
+    @Delete
+    suspend fun delete(item: Item)
+
+    @Query("SELECT * from items WHERE id = :id")
+    fun getItem(id: Int): Flow<Item>
+
+    @Query("SELECT * from items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
+
+}
