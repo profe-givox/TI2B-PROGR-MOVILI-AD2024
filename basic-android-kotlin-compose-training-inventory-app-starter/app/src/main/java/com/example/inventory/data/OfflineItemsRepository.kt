38b063c418f44aa99d7da82a2bb151c05330d6ca
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-10-23 18:03:52 -0600

    Se agrego repository, se implemento corrutinas y se agrego el codigo que inserta Item

--- basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/data/OfflineItemsRepository.kt
@@ -16,4 +16,22 @@
 
 package com.example.inventory.data
 
-class OfflineItemsRepository : ItemsRepository
+import kotlinx.coroutines.flow.Flow
+
+class OfflineItemsRepository(private val itemDao: ItemDao) : ItemsRepository {
+    override fun getAllItemsStream(): Flow<List<Item>> {
+        return itemDao.getAllItems()
+    }
+
+    override fun getItemStream(id: Int): Flow<Item?> {
+        return itemDao.getItem(id)
+    }
+
+    override suspend fun insertItem(item: Item)= itemDao.insertPerro(item)
+
+    override suspend fun deleteItem(item: Item) = itemDao.delete(item)
+
+    override suspend fun updateItem(item: Item) {
+        return itemDao.perroActualizar(item)
+    }
+}
--- basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
+++ basic-android-kotlin-compose-training-inventory-app-starter/app/src/main/java/com/example/inventory/ui/item/ItemEntryScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
@@ -45,6 +46,7 @@ import com.example.inventory.R
 import com.example.inventory.ui.AppViewModelProvider
 import com.example.inventory.ui.navigation.NavigationDestination
 import com.example.inventory.ui.theme.InventoryTheme
+import kotlinx.coroutines.launch
 import java.util.Currency
 import java.util.Locale
 
@@ -61,6 +63,7 @@ fun ItemEntryScreen(
     canNavigateBack: Boolean = true,
     viewModel: ItemEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
+    val coroutineScope = rememberCoroutineScope()
     Scaffold(
         topBar = {
             InventoryTopAppBar(
@@ -73,7 +76,12 @@ fun ItemEntryScreen(
         ItemEntryBody(
             itemUiState = viewModel.itemUiState,
             onItemValueChange = viewModel::updateUiState,
-            onSaveClick = { },
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveItem()
+                    navigateBack()
+                }
+            },
             modifier = Modifier
                 .padding(
                     start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
